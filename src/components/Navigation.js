27fe1e0bc17657d8114eb04c9ad95d127f981035
WolfSpiderman <wolfspiderman@yahoo.com>
2023-06-24 17:17:19 -0500

    added Navigation component inside Header

---
+++ src/components/Navigation.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+
+export default function Navigation({ currentPage, handlePageChange }) {
+  return (
+    <Navbar expand="md" bg="dark" variant="dark">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto" activeKey={currentPage}>
+          <Nav.Link eventKey="About" onClick={() => handlePageChange('About')}>
+            About
+          </Nav.Link>
+          <Nav.Link eventKey="Portfolio" onClick={() => handlePageChange('Portfolio')}>
+            Portfolio
+          </Nav.Link>
+          <Nav.Link eventKey="Resume" onClick={() => handlePageChange('Resume')}>
+            Resume
+          </Nav.Link>
+          <Nav.Link eventKey="Contact" onClick={() => handlePageChange('Contact')}>
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
