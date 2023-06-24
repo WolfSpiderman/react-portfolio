60bb58795b8f6d29f5d31d6cd7e0f92c30dc1482
WolfSpiderman <wolfspiderman@yahoo.com>
2023-06-24 17:17:42 -0500

    added PageContainer to use for renderPage stuff

---
+++ src/components/PageContainer.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
+
+export default function PageContainer() {
+    const [currentPage, setCurrentPage] = useState('About');
+
+    const renderPage = () => {
+        switch (currentPage) {
+          case 'About':
+            return <About />;
+          case 'Portfolio':
+            return <Portfolio />;
+          case 'Resume':
+            return <Resume />;
+          case 'Contact':
+            return <Contact />;
+          default:
+            return <About />;
+        }
+      };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+            {renderPage()}
+            <Footer />
+        </div>
+    )
+}
