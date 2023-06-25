93f7d0bc6014b6b9faef06d904746dcb5c7bdcea
WolfSpiderman <wolfspiderman@yahoo.com>
2023-06-25 18:58:07 -0500

    think i got resume download link working

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 export default function Resume() {
-    const resumePDF = '../../../public/JoshuaJenkins_Resume.pdf';
+  const resumePDF = `${process.env.PUBLIC_URL}/JoshuaJenkins_Resume.pdf`;
 
-    return (
-        <main>
-            <a href={resumePDF} download="JoshuaJenkins_Resume.pdf">Download my Resume!</a>
+  return (
+    <main>
+      <a href={resumePDF} download="Joshua_Jenkins_Resume.pdf">
+        Download my Resume!
+      </a>
 
-            <ul>
-                <li></li>
-            </ul>
-        </main>
-    )
+      <ul>
+        <li></li>
+      </ul>
+    </main>
+  );
 }
