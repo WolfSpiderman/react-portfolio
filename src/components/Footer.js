1082afe9b11c0db73ffaee6e7f17a6ac8d6a6899
WolfSpiderman <wolfspiderman@yahoo.com>
2023-06-27 21:43:18 -0500

    added stackoverflow link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { BiLogoGithub, BiLogoLinkedin } from 'react-icons/bi';
+import { FaStackOverflow } from 'react-icons/fa';
 
 export default function Footer() {
   return (
     <footer className='d-flex flex-row justify-content-center align-items-center' style={{ marginTop: 40, height: 200, backgroundImage: 'linear-gradient(144deg, #ecc66c, #4e5885 45%, #bc5032)' }}>
-      <a href="https://github.com/WolfSpiderman">
+      <a href="https://github.com/WolfSpiderman" style={{ marginRight: 15 }}>
         <BiLogoGithub style={{ minHeight: 100, minWidth: 100, color: '#61b0bb' }}/>
       </a>
       
-      <a href="https://www.linkedin.com/in/joshua-jenkins-4960aa259">
+      <a href="https://www.linkedin.com/in/joshua-jenkins-4960aa259" style={{ marginRight: 15 }}>
         <BiLogoLinkedin style={{ minHeight: 100, minWidth: 100, color: '#61b0bb' }}/>
+      </a>
+
+      <a href="https://stackoverflow.com/users/22142526/joshua-jenkins">
+        <FaStackOverflow style={{ minHeight: 100, minWidth: 100, color: '#61b0bb' }} />
       </a>
     </footer>
   );
