37e471b3982a0c0a67942c163ce216c3176acf3a
WolfSpiderman <wolfspiderman@yahoo.com>
2023-06-27 21:12:57 -0500

    'Finished' contact page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PageContainer from './components/PageContainer';
-import './App.css';
 
 function App() {
   return (
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -6,12 +6,23 @@ export default function Contact() {
     email: '',
     message: ''
   });
-  
+
   const [errorMessage, setErrorMessage] = useState('');
 
   const handleInputChange = event => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
+    setErrorMessage(''); // Clear the error message when input changes
+  };
+
+  const handleInputBlur = event => {
+    const { name, value } = event.target;
+
+    if (!value) {
+      setErrorMessage(`Please enter your ${name}`);
+    } else if (name === 'email' && !/\S+@\S+\.\S+/.test(value)) {
+      setErrorMessage('Please enter a valid email address');
+    }
   };
 
   const handleContactFormSubmit = event => {
@@ -38,15 +49,15 @@ export default function Contact() {
       {errorMessage && <p className="text-danger">{errorMessage}</p>}
       <form className="d-flex flex-column align-items-center justify-content-around" onSubmit={handleContactFormSubmit}>
         <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" value={formData.name} onChange={handleInputChange}/>
+        <input type="text" id="name" name="name" value={formData.name} onChange={handleInputChange} onBlur={handleInputBlur} onFocus={() => setErrorMessage('')} />
 
         <label htmlFor="email">Email:</label>
-        <input type="text" id="email" name="email" value={formData.email} onChange={handleInputChange}/>
+        <input type="text" id="email" name="email" value={formData.email} onChange={handleInputChange} onBlur={handleInputBlur} onFocus={() => setErrorMessage('')} />
 
         <label htmlFor="message">Message:</label>
-        <textarea id="message" name="message" value={formData.message} onChange={handleInputChange}/>
+        <textarea id="message" name="message" value={formData.message} onChange={handleInputChange} onBlur={handleInputBlur} onFocus={() => setErrorMessage('')} />
 
-        <button type="submit">Submit</button>
+        <button type="submit" style={{ marginTop: 10 }}>Submit</button>
       </form>
     </main>
   );
