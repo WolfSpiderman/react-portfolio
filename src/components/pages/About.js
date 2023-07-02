8da4b7f6d377deba288640d0176be0f80dce94ee
WolfSpiderman <wolfspiderman@yahoo.com>
2023-07-02 00:39:56 -0500

    fixed typos

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -19,7 +19,7 @@ export default function About() {
                     But what truly excites me is the opportunity to leverage my knowledge and experience. I take pride in developing websites using cutting-edge technologies, and in this context, I am proud to share that this very portfolio page has been created almost exclusively with React.
                     <br />
                     <br />
-                    In building this portfolio, I have aimed to showcase my diverse skillset while ensuring an enjoyable and seemless user experience. Please explore my work and projects, and feel free to reach out for any collaboration opportunities or queries. I look forward to connecting with like-minded individuals and organizations, to create meaningful digital experiences that make a positive impact.
+                    In building this portfolio, I have aimed to showcase my diverse skill-set while ensuring an enjoyable and seamless user experience. Please explore my work and projects, and feel free to reach out for any collaboration opportunities or queries. I look forward to connecting with like-minded individuals and organizations, to create meaningful digital experiences that make a positive impact.
                     <br />
                     <br />
                     Thank you for visiting my portfolio website, and I hope you have a great user experience!
